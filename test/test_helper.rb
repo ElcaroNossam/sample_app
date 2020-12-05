b8783f086e5320792b9c23ef68267c2033fb3c77
Elcaro Nossam <agent_shops@i.ua>
2020-12-06 00:23:25 +0200

    sessions && cookies

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -11,5 +11,30 @@ class ActiveSupport::TestCase
   fixtures :all
   include ApplicationHelper
 
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+
+   # Осуществляет вход тестового пользователя
+   def log_in_as(user, options = {})
+   password    = options[:password]    || 'password'
+   remember_me = options[:remember_me] || '1'
+   if integration_test?
+     post login_path, params: { session: { email:       user.email,
+                                 password:    password,
+                                 remember_me: remember_me } }
+   else
+     session[:user_id] = user.id
+   end
+ end
+
+  private
+
+  # Возвращает true внутри интеграционных тестов
+  def integration_test?
+    defined?(follow_redirect!)
+  end
+
+
   # Add more helper methods to be used by all tests here...
 end
